8f57bb1fe25cc6f290f276458936f52f89511d11
FiVo <finn.volkel@gmail.com>
2019-06-11 19:12:43 +0200

    add depth

--- crawler.lisp
+++ crawler.lisp
@@ -14,17 +14,23 @@
                 #:enqueue-in-sec)
   (:import-from #:vom)
   (:export #:crawler
+           #:crawler-depth
            #:crawler-user-agent))
 (in-package #:ragno/crawler)
 
 (defclass crawler (spider psy:worker)
-  ((user-agent :initarg :user-agent
+  ((depth :initarg :depth
+          :initform -1
+          :accessor crawler-depth)
+   (user-agent :initarg :user-agent
                :initform "Ragno-Crawler"
                :accessor crawler-user-agent)))
 
 (defmethod psy:perform ((crawler crawler) &rest args)
   (let ((uri (first args))
+        (depth (second args))
         (*user-agent* (crawler-user-agent crawler)))
+    (setf (crawler-depth crawler) depth)
     (handler-case
         (scrape crawler uri)
       (ragno-concurrency-limit (e)
